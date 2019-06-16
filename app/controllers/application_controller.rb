ccae93c2a80f3e8150af14f0246aa7f7e38ec321
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-06-16 16:22:15 +0900

    feat: Bootstrap導入 + ページモックアップ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def show
-    render html:"show"
-  end
-
-  def new
-    render html:"new"
-  end
-
-  def edit
-    render html:"edit"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resource :tasks
+  root 'tasks#show'
+
+  get 'tasks/edit'
+  get 'tasks/new'
 end
