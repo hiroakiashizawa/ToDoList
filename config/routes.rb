f30cb8ccf961b748c21980381c1f0bc216a07842
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-06-17 20:50:38 +0900

    fix: ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resource :tasks
   root 'tasks#index'
 
+  get 'tasks', to: 'tasks#index'
   get 'tasks/complete'
   get 'tasks/edit'
   get 'tasks/new'
