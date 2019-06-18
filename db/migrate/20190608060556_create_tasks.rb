1d8cc9a1603549a29eb77cc04e2900a583ccc678
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-06-18 16:04:30 +0900

    Revert "fix: データベース再定義 (user_id :追加, limit: 削除)"
    This reverts commit 47589f60097b8bba2938e534946157adecd1622b.

--- db/migrate/20190608060556_create_tasks.rb
+++ db/migrate/20190608060556_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :title
       t.text :content
-      t.references :user, foreign_key: true
+      t.datetime :limit
 
       t.timestamps
     end
