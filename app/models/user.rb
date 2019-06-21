98cc6af2a32e77cecccb5c1ef4f6ab61ade59d22
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-06-21 19:30:32 +0900

    test: Modelへ単体テスト用バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :tasks
+
+  with_options presence: true do
+    validates :name
+    validates :email
+  end
+
+  with_options uniqueness: true do
+    validates :email
+  end
+
 end
