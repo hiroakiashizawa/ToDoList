b47e8c548a934d999472c23e5ec4c085ba52a22e
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-07-01 22:46:29 +0900

    feature: complete画面に完了状態(completed: true)にしたTaskを表示できるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_080835) do
+ActiveRecord::Schema.define(version: 2019_07_01_125804) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_06_29_080835) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "timelimit"
+    t.boolean "completed", default: false, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
