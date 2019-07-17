74504695498dc47a87148ed61e7ad75db8ea7988
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-07-17 21:35:43 +0900

    test: [users]単体テスト(正常系、異常系)修正

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,59 +3,49 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   include SessionsHelper
 
-  describe "#index" do
-
-    before do
-      @user = FactoryBot.create(:user)
-    end
+  describe "GET #index" do
+    let(:user) { FactoryBot.create(:user) }
 
     it "responds successfully" do
-      log_in(@user)
+      log_in(user)
       get :index
       expect(response).to be_success
     end
   end
 
-  describe '#create' do
+  describe 'POST #create' do
+    let(:user_params) { FactoryBot.attributes_for(:user) }
 
     it "add a user" do
-      users_params = FactoryBot.attributes_for(:user)
       expect {
-        post :create, params: { user: users_params }
+        post :create, params: { user: user_params }
       }.to change{ User.count }.by(1)
     end
 
     it "redirectes to users/index page" do
-      users_params = FactoryBot.attributes_for(:user)
-      post :create, params: { user: users_params }
+      post :create, params: { user: user_params }
       expect(response).to redirect_to "/users"
     end
   end
 
-  describe "#update" do
-
-    before do
-      @user = FactoryBot.create(:user)
-    end
+  describe "PATCH #update" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:user_params) { FactoryBot.attributes_for(:user, name: "hiroaki") }
 
     it "update a user" do
-      log_in(@user)
-      users_params = FactoryBot.attributes_for(:user, name: "hiroaki")
-      patch :update, params: { id: @user.id, user: users_params }
-      expect(@user.reload.name).to eq "hiroaki"
+      log_in(user)
+      patch :update, params: { id: user.id, user: user_params }
+      expect(user.reload.name).to eq "hiroaki"
     end
   end
 
-  describe "#destroy" do
-
-    before do
-      @user = FactoryBot.create(:user)
-    end
+  describe "DELETE #destroy" do
+    let(:user) { FactoryBot.create(:user) }
 
     it "destroy a user" do
-      log_in(@user)
+      log_in(user)
       expect {
-        delete :destroy, params: { id: @user.id }
+        delete :destroy, params: { id: user.id }
       }.to change{ User.count }.by(-1)
     end
   end
