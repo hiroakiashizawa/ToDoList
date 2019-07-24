7f8aa058594abbd44c4b19604b365f71363a2ece
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-07-24 22:33:02 +0900

    feature: 検索機能実装　※テストはうまくいってない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   include TasksHelper
   include SessionsHelper
   include AdminHelper
+  before_action :search_tasks
+
+  def search_tasks
+    @search = current_user.tasks.all.ransack(params[:q])
+    @search_tasks = @search.result
+  end
 end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,7 @@
 module AdminHelper
+
+  def search_tasks
+    @search = current_user.tasks.all.ransack(params[:q])
+    @search_tasks = @search.result
+  end
 end
