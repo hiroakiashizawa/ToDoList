9f39cd5a38b517600442f71a8e10f842ee79923a
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-08-02 23:22:37 +0900

    Revert "feature: 管理者コンソールにTaskグラフ追加"
    This reverts commit 471a50016354b5062a53b76dd2bc31375ecf69ba.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,3 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-//= require chartkick
-//= require Chart.bundle
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,6 @@ class AdminController < ApplicationController
   before_action :require_login, :require_admin
 
   def index
-    @tasks = Task.all
   end
 
   def users_show
