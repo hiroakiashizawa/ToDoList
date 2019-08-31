cd71a80fb1dd986d70be3f20ec60a0c0f2fc348e
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-08-31 17:06:57 +0900

    test: Projectコントローラテスト作成[WIP]

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,124 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  include SessionsHelper
 
+  describe 'GET #index' do
+    let(:user) { FactoryBot.create(:user_anti_dup_email) }
+
+    context "when logged in" do
+
+      it "responds successfully" do
+        log_in(user)
+        get :index
+        expect(response).to be_success
+      end
+    end
+
+    context "when not logged in" do
+
+      it "responds not successfully" do
+        get :index
+        expect(response).not_to be_success
+      end
+    end
+  end
+
+  describe 'GET #new' do
+    let(:user) { FactoryBot.create(:user_anti_dup_email) }
+
+    context "when logged in" do
+
+      it "responds successfully" do
+        log_in(user)
+        get :new
+        expect(response).to be_success
+      end
+    end
+
+    context "when not logged in" do
+
+      it "responds not successfully" do
+        get :new
+        expect(response).not_to be_success
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:user) { FactoryBot.create(:user_anti_dup_email) }
+    let(:project) { FactoryBot.create(:project) }
+
+
+    context "when logged in" do
+
+      it "responds successfully" do
+        log_in(user)
+        get :edit, params: { id: project.id } 
+        expect(response).to be_success
+      end
+    end
+
+    context "when not logged in" do
+
+      it "responds not successfully" do
+        get :edit, params: { id: project.id } 
+        expect(response).not_to be_success
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+
+    before do
+    end
+
+    let(:user) { FactoryBot.create(:user_anti_dup_email) }
+    let(:project) { FactoryBot.create(:project, user_id: user) }
+    let(:project2) { FactoryBot.create(:project2) }
+    let(:project_params) { FactoryBot.attributes_for(:project, user: user) }
+
+    context "when logged in" do
+
+      it "updates successfully" do
+        log_in(user)
+        patch :update, params: { id: project.id, project: project_params }
+        expect(project.reload.project_name).to eq "all"
+        binding.pry
+      end
+    end
+
+    context "when not logged in" do
+
+      it "updates not successfully" do
+        patch :update, params: { id: project.id, projects: { project_name: "all"} } 
+        expect(project.reload.project_name).not_to eq "all"
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+
+    before do
+      @project = FactoryBot.create(:project)
+    end
+
+    let(:user) { FactoryBot.create(:user_anti_dup_email) }
+    let(:delete_request) { delete :destroy, params: { id: @project.id, project: @project } }
+
+    context "when logged in" do
+
+      it "is deleted successfully" do
+        log_in(user)
+        expect{ delete_request }.to change(Project, :count)
+      end
+    end
+
+    context "when not logged in" do
+
+      it "is deleted not successfully" do
+        expect{ delete_request }.not_to change(Project, :count)
+      end
+    end
+  end
 end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,19 @@
 FactoryBot.define do
   factory :project do
-    id { 1 }
-    project_name { "MyString" }
+    id { 50 }
+    project_name { "WaoWao" }
+    user_id { @user }
+    
+    factory :project_2 do
+      id { 2 }
+    end
+
+    factory :project_3 do
+      id { 3 }
+    end
+
+    factory :project_params do
+      project_name { "all" }
+    end
   end
 end
