c39ede4c93bc86878d1932ef91b016ff6899181d
hiroakiashizawa <jo.ultim.layco@gmail.com>
2019-10-05 09:38:20 +0900

    feature: Project個別画面作成中

--- app/controllers/projects/tasks_controller.rb
+++ app/controllers/projects/tasks_controller.rb
@@ -1,9 +1,10 @@
 class Projects::TasksController < ApplicationController
 
+  before_action :require_login
+
   def show
     @project = Project.find(params[:project_id])
-    @task = @project.tasks.where(params[:id])
-    binding.pry
+    @task = @project.tasks.find(params[:id])
   end
 
   def index
@@ -23,4 +24,13 @@ class Projects::TasksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def require_login
+      unless logged_in?
+        flash[:danger] = "Please login!"
+        redirect_to login_path
+      end
+    end
 end
